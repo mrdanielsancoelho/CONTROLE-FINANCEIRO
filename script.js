5e3bdbc7915a476c79cafb687504cbeff5baede4
Daniel Coelho <danslord2020@gmail.com>
2021-09-24 20:18:17 -0300

    pus os valores no Dom

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
+const transactionsUL = document.querySelector('#transactions')
+
+
 const dummyTransactions = [
-    { id: 1, name: 'digispark ar1', amount: -20 },
-    { id: 2, name: 'digispark ar2', amount: 300 },
-    { id: 3, name: 'digispark ar3', amount: -10 },
-    { id: 4, name: 'digispark ar4', amount: 150 }
+    { id: 1, name: 'Digispark ar1', amount: -20 },
+    { id: 2, name: 'Bateria', amount: 300 },
+    { id: 3, name: 'Controle', amount: -10 },
+    { id: 4, name: 'Camera', amount: 150 }
 ]
 
 //Vamos adicionar as transações ao dom
 
 const addTransactionIntoDom = transaction => {
     const operator = transaction.amount < 0 ? '-' : '+'
-    console.log(operator) 
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+    
 
-    // < li class="minus" >
-    //     Salário <span> -$400</span> <button class="delete-btn">x</button>
-    // </li>
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+        ${transaction.name} <span> ${operator}R$${amountWithoutOperator}</span> <button class="delete-btn">x</button>
+    `
+    transactionsUL.append(li)
 }
 
-addTransactionIntoDom(dummyTransactions(0))
+const updateBalanceValues = () => {
+    const transactionsAmounts = dummyTransactions.map(transaction => transaction.amount)
+    console.log(transactionsAmounts)
+}
+
+const init = () => {
+    dummyTransactions.forEach(addTransactionIntoDom)
+    updateBalanceValues()
+}
+init()
